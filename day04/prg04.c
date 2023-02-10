ef1f9a0e4bb3f041c36fedd76d8635075a9f25dc
bhimatak <bhima.t@gmail.com>
2023-02-10 16:59:09 +0530

    updated

---
+++ day04/prg04.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char str[100];   // = "This is to test string"; //dec with initialisation
+
+	//char *str1;
+
+	strcpy(str,"This is a test"); 
+
+	printf("\n%s",str);
+
+	printf("\n\n");
+
+
+
+	return 0;
+}
