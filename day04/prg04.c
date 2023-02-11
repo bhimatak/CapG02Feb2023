dd62d0fc850b1f6e17ab8e48cf68b8b6bdd94dd3
bhimatak <bhima.t@gmail.com>
2023-02-11 09:25:12 +0530

    updated

--- day04/prg04.c
+++ day04/prg04.c
@@ -3,13 +3,20 @@
 
 int main()
 {
-	char str[100];   // = "This is to test string"; //dec with initialisation
-
+	char str[100];// = "This is to test string"; //dec with initialisation
+	int len = 0;
 	//char *str1;
 
-	strcpy(str,"This is a test"); 
+	//strcpy(str,"This is a test");
 
-	printf("\n%s",str);
+	fgets(str, 1024, stdin);
+
+	len = strlen(str);
+
+	printf("\n%s and its length=%d",str,len);
+
+	printf("\nstr[0]=%c \t str[1]=%c \n",str[0],str[1]);
+	printf("\nstr[strlen-1]=%c and its ascii value=%d", str[len-1], str[len-1]);
 
 	printf("\n\n");
 
