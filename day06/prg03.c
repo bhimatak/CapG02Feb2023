9faceacacf6a3efa8e1ccdabd2c7152c744c040b
bhimatak <bhima.t@gmail.com>
2023-02-13 14:46:06 +0530

    Updated

--- day06/prg03.c
+++ day06/prg03.c
@@ -23,18 +23,23 @@ int main()
 {
 
 	int gnum=0;
-	int rnum=0, rem=0, temp=0;
+	int rnum=0, rem, temp=0;
+	int count;
 
 	printf("\nEnter the given number: ");
 	scanf("%d",&gnum);
+
 	
-	for(temp = gnum;temp!=0;(temp = temp / 10))
+	for(temp = gnum,rem=0, count=1;((temp!=0)&&(count<=3)) ;(temp = temp / 10),count++)
 	{
+		
 		rem = temp%10;
 		rnum = (rnum * 10)+rem;
+		
 	}
 
 	printf("\nGiven No: %d\nRev No: %d\n",gnum,rnum);
+	printf("\n%d times the loop has looped\n\n",count-1);
 
 
 	return 0;
