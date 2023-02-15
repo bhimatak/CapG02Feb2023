277d4da3732c77c93129221ec02ba22a075d5367
bhimatak <bhima.t@gmail.com>
2023-02-15 17:11:32 +0530

    Updated

--- day08/prg06.c
+++ day08/prg06.c
@@ -14,12 +14,12 @@ int revArr(int [], int);
 
 int main()
 {
-	int a[5] = {10,20,30,40,50};
+	int a[6] = {10,20,30,40,50,60};
 
-	printList(a,5);
-	revArr(a,5);
+	printList(a,6);
+	revArr(a,6);
 	printf("\nAfter Reversing the array\n");
-	printList(a,5);
+	printList(a,6);
 	
 	return 0;
 }
@@ -36,15 +36,16 @@ void printList(int arr[], int n)
 
 int revArr(int arr[], int n)
 {
-	int i,j;
+	int i;
 	int temp;
-	for(i=0;i<n;i++)
+	int si, ei, mid=n/2;
+	
+	for(si=0,ei=n-1;si<mid;si++)
 	{
-		for(j=n-1;j>=0;j--)
-		{
-			temp = arr[j];
-			arr[j] = arr[i];
-			arr[i]=temp;
-		}
+		temp = arr[ei];
+		arr[ei] = arr[si];
+		arr[si]=temp;
+		ei--;
+	
 	}
 }
