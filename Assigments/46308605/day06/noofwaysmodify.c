989aa8c0efe1b3f577bb142a94a842727bfa1cb3
Akash <akash@ubuntu20.04.myguest.virtualbox.org>
2023-02-15 23:03:43 +0530

    noofwaymodify

---
+++ Assigments/46308605/day06/noofwaysmodify.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int num, count = 0;
+    int i,j,k;
+    printf("Enter a number ");
+    scanf("%d", &num);
+    for (i = 1;i<=num;i++)
+    {
+        for (j=i;j<=num;j++)
+        {
+            for ( k =j;k<=num;k++)
+            {
+                int l = num - i - j - k;
+                if (l >= k && l <= num - i - j)
+                {
+                    count++;
+                    printf("%d + %d + %d + %d\n", i, j, k, l);
+                }
+            }
+        }
+    }
+    printf("Count: %d\n", count);
+    return 0;
+}
