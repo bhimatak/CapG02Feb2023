0611a6039fb1acd3dc228a22dd3fdb01bf93e8ae
AAravind7 <avinashmetta171@gmail.com>
2023-02-16 16:22:35 +0530

    updated

---
+++ Assigments/46307913/sortD.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int sort(int n,int *ptr)
+{
+    int i,j,temp;
+	for(i=0;i<n;i++)
+	{
+	    for(j=i+1;j<n;j++)
+		{
+		    if(*(ptr+j)<*(ptr+i))
+			{
+			   temp=*(ptr+i);
+			   *(ptr+i)=*(ptr+j);
+			   *(ptr+j)=temp;
+	   	    }
+	    }
+    }
+	for(i=0;i<n;i++)
+	    printf("\n%d\n",*(ptr+i));
+}
+int main()
+{
+    int n=5;
+	int arr[]={10,20,30,1,19};
+	sort(n,arr);
+	return 0;
+}
