b9b9597efa7b1e19cd92957b56be34c10dec7e3d
Srivathsan06 <srivathsan06@gmail.com>
2023-02-16 10:55:46 +0000

    String_length_using_pointer

---
+++ Assigments/46307914/feb16/stringLenPointer.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+void lenWithPointer(char name[1024]);
+
+int main()
+{
+	char name[1024];
+	char *ptr;
+	ptr=name;
+
+	printf("\n\nEnter your name: ");
+	scanf("%s",ptr);
+
+	lenWithPointer(name);
+
+	return 0;
+}
+
+void lenWithPointer(char name[1024])
+{
+	int i;
+	char *ptr;
+	ptr=name;
+	for(i=0;;i++)
+	{
+		if(*(ptr+i)=='\0')
+		{
+			break;
+		}
+	}
+	printf("\n %d is the length of your name\n\n",i);
+}
