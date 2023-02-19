0051cda7fef5485f4279d77c34920321878b98e1
jothimani2k <jomanjit2k@gmail.com>
2023-02-19 21:07:35 +0530

    updatted

---
+++ Assigments/46307923/sort.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main()
+{
+	int arr[5],a;
+	int *ptr;
+	int i,j;
+
+	ptr= arr;
+
+	for(i=0;i<5;i++)
+	{
+		scanf("%d",(ptr+i));
+    }
+
+	for(i=0;i<5;i++)
+    {
+		for(j=i+1;j<5;j++)
+		{
+			if(arr[i]<arr[j])
+			{
+				a= arr[i];
+				arr[i] = arr[j];
+				arr[j] = a;
+            }
+        }
+    }
+	printf("In decending order;");
+	for (i=0;i<5;i++)
+	{
+		printf(" %d",*(ptr+i));
+    }
+
+
+}
+
