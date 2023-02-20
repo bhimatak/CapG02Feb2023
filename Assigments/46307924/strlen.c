3e53aa178c7ac5a0b9d8792e84ae697dc933ab75
abishekkumar23 <k.abishekkoilpitchai2001@gmail.com>
2023-02-21 00:29:19 +0530

    Completed

---
+++ Assigments/46307924/strlen.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+	char str[100];
+	int i,count=0;
+	char *str;
+	printf("Enter the string : ");
+	ptr=str;
+	gets(ptr);
+	
+	for(i=0;str[i]!='\0';i++)
+	{
+		count++;
+	}
+
+	printf("The length of the string is : %d",count);
+	return 0;
+}
+
+
+
+
