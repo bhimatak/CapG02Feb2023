3c36710be2026be5fb8663be445362d054ca7203
sourav2903roy <souravroy382@gmail.com>
2023-02-21 10:27:44 +0530

    Uploading Assignment 1

--- Assigments/46308518/Assign1_Nalanda.c
+++ Assigments/46308518/Assign1_Nalanda.c
@@ -6,10 +6,13 @@ int main(){
 	// to input
 	for(int j=0;j<10;j++)
 		scanf("%d",&arr[j]);
-	printf("Enter num. of elementts to display");
+	printf("\nEnter num. of elementts to display");
 	scanf("%d",&n);
 	
 	printArray(arr,n);
+
+	printf("\n\nThe sum of smallest and largest number is: %d", sum_Min_Max(arr));
+
 	return 0;
 }
 
@@ -19,4 +22,21 @@ void printArray(int arr[],int n){
 	for(int i=0;i<n;i++){
 		printf("%d\t",arr[i]);
 	}
+}
+
+
+int sum_Min_Max(int arr[]){
+	int temp=0;
+	for(int k=0;k<10;k++){
+		for(int l=k+1;l<10;l++){
+			if(arr[k]>arr[l]){
+				temp=arr[k];
+				arr[k]=arr[l];
+				arr[l]=temp;
+			}
+		}
+	}
+
+	int res=arr[0]+arr[9];
+	return res;
 }
