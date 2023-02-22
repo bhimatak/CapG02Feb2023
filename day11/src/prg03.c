213e882cf3042698c5a5b73fff8bcc1af529edcc
bhimatak <bhima.t@gmail.com>
2023-02-22 11:16:49 +0530

    Updated

---
+++ day11/src/prg03.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char dName[20];
+	char sName[20];
+
+	memset(dName,'\0',20);
+	memset(sName,'\0',20);
+
+	printf("\nEnter SName: ");
+	scanf("%s",sName);
+
+	printf("\nEnter dName: ");
+	scanf("%s",dName);
+
+	printf("\nsName: %s\ndName: %s\n",sName,dName);
+	
+	//memset(sName,'\0',20);
+
+	//strcat(dName,"\r");
+	strcat(dName,sName);
+
+	printf("\nsName: %s\ndName: %s\n",sName,dName);
+
+	printf("\n\n");
+
+	return 0;
+}
+
