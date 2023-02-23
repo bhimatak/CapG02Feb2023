f0ae48df46ce586984d0289193e13c0945466f46
bhimatak <bhima.t@gmail.com>
2023-02-23 14:55:15 +0530

    Updated

--- day12/inc/myHeader.h
+++ day12/inc/myHeader.h
@@ -1,4 +1,9 @@
 /* first para => Base address of the array, second para size of the array */
+/* for integer arrays */
+#include <string.h>
+
+#define BUFF 1024
+
 void dispArr(int *, int);
 
 void getValArr(int *, int);
@@ -6,3 +11,13 @@ void getValArr(int *, int);
 void sortAArr(int *, int);
 
 void sortDArr(int *, int);
+
+/* array function for strings */
+
+void dispList(char **, int);
+
+char **setList(char **, int);
+
+char **sortStrAsc(char **, int);
+
+char **sortStrDsc(char **, int);
