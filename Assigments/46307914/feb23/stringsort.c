1ebeaa5600582bfc605cc948dc1f90ed3a57fb07
Srivathsan06 <srivathsan06@gmail.com>
2023-02-23 09:50:54 +0000

    updated

--- Assigments/46307914/feb23/stringsort.c
+++ Assigments/46307914/feb23/stringsort.c
@@ -1,23 +1,47 @@
 #include<stdio.h>
 #include<string.h>
+#define SIZE 1024
+#define SIZE2 1024
+
+int inputfunc(char [SIZE][SIZE2]);
+void sortingstring(char [SIZE][SIZE2] ,int);
 
 int main()
 
 {
-	char empnames[1024][1024];
-	char temp[1024];
-	int j=0,i=0,n=0;
+	char empnames[SIZE][SIZE2];
+	int n=0;
+	
+	n= inputfunc(empnames);
 
+	sortingstring(empnames,n);
+
+	return 0;
+
+}
+	
+int inputfunc(char empnames[SIZE][SIZE2])
+{
+	int n,i=0;
+	
 	printf("\nEnter the number of employees: ");
 	scanf("%d",&n);
 	
 	for(i=0;i<n;i++)
 	{
-		printf("\nEnter the names of employee%d: ",i);
+		printf("\nEnter the names of employee%d: ",i+1);
 		scanf(" ");
 		fgets(empnames[i],1024,stdin);
 		empnames[i][strlen(empnames[i])-1]='\0';
 	}
+
+	return n;
+}
+
+void sortingstring( char empnames[SIZE][SIZE2],int n)
+{
+	char temp[SIZE];
+	int i=0,j=0;
 
 	printf("\nSorted List\n");
 
@@ -42,7 +66,6 @@ int main()
 
 	printf("\n\n");
 
-	return 0;
 
 }
 			
