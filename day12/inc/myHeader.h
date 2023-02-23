96abfd7ab6763938d9a8bee1ad7e6f10bb892de3
bhimatak <bhima.t@gmail.com>
2023-02-23 12:33:48 +0530

    updated

---
+++ day12/inc/myHeader.h
@@ -0,0 +1,8 @@
+/* first para => Base address of the array, second para size of the array */
+void dispArr(int *, int);
+
+void getValArr(int *, int);
+
+void sortAArr(int *, int);
+
+void sortDArr(int *, int);
