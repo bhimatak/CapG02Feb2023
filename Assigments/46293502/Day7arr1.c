e37ccaa34bf897bd70c75b03b3e0cbc7792fda04
MBAURI <mrityunjoy21@gmail.com>
2023-02-24 18:51:13 +0000

    array

---
+++ Assigments/46293502/Day7arr1.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+	int arr[1024];
+        int range,i,j,temp=0;
+	printf("\n\n enter size of array: \n\n");
+	scanf("%d",&range);
+	printf("\n\n enter the array elements: \n");
+	for(i=0;i<range;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<range;i++)
+	{
+		for(j=i;j<range-1;j++)
+		{
+			arr[j]=arr[j+1];
+		}
+
+	}
+	printf("\n output:\n");
+	for(i=0;i<range/2;i++)
+	{
+		printf("\n%d",arr[i]);
+	}
+	printf("\n\n");
+	return 0;
+}
+
+
+
+
