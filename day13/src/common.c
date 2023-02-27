6b256d439ee2758cfacef0d78ee6bbcca58b89e1
bhimatak <bhima.t@gmail.com>
2023-02-27 17:25:03 +0530

    Updated

--- day13/src/common.c
+++ day13/src/common.c
@@ -24,3 +24,15 @@ int signIn(PERSON *p, LOGIN *l,int *pid)
 
 	return flag;
 }
+
+int tktBkgMenu()
+{
+	int ch;
+	printf("\nPress,");
+	printf("\n1. Book Ticket");
+	printf("\n2. Print Tiket");
+	printf("\n3. Go Back to Main Menu");
+	printf("\n\nChoice: ");
+	scanf("%d",&ch);
+	return ch;
+}
