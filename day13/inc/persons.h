e5cf06840e525cafa3be8a50c262eaa82cd488f2
bhimatak <bhima.t@gmail.com>
2023-02-27 12:56:11 +0530

    Updated

---
+++ day13/inc/persons.h
@@ -0,0 +1,15 @@
+struct Person
+{
+	int pid,
+	char name[20];
+	char emailid[20];
+	int phno;
+	char gender;
+	int uid;
+	char passwd[20];
+	int age;
+	char address[50];
+};
+
+typedef struct Person PERSON;
+
