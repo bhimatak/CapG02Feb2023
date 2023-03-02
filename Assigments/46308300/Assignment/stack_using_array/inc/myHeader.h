2a477c26f54e7378be2f4d1b24590e4e20d43d7b
Princ398ku <kprince82549@gmail.com>
2023-03-02 16:56:01 +0000

    added assignment

--- Assigments/46308300/Assignment/stack_using_array/inc/myHeader.h
+++
@@ -1,8 +0,0 @@
-#define MAX 5
-
-void push(int *,int *);
-void pop(int *,int *);
-void display(int *,int *);
-int isEmpty(int *);
-int isFull(int *);
-void peek(int *, int *);
--- Assigments/46308300/Assignment/stk_using_array/inc/myHeader.h
+++
@@ -1,6 +0,0 @@
-#define size 20
-
-int getInput(int [],int *);
-int isFull(int);
-int isEmpty(int);
-void reverseDisplay(int [],int);
