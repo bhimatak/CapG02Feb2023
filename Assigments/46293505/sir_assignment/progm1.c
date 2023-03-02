56b02ac0403e2946d135208b6ba8dedecbe439a3
shashankrohith <rohithsachinv23@gmail.com>
2023-03-02 05:57:05 +0000

    progm1.c

---
+++ Assigments/46293505/sir_assignment/progm1.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct Employee
+{
+	int id;
+	char name[20];
+};
+
+typedef struct Employee EMP;
+
+int printDetails(EMP);
+
+int main(int argc,char **argv)
+{
+	EMP emp;
+	strcpy(emp.name,argv[1]);
+	emp.id=atoi(argv[2]);
+	printDetails(emp);
+
+
+	return(EXIT_SUCCESS);
+}
+int printDetails(EMP emp)
+{		
+	printf("\nemployee id: %d",emp.id);
+	printf("\nemployee name: %s\n",emp.name);
+
+	
+}
+
+
+
+
+
+
