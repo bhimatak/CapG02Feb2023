e07b1505aa8af0f8df5a47668c0befb57c4786a4
jothimani2k <jomanjit2k@gmail.com>
2023-03-02 16:26:17 +0000

    updated

---
+++ Assigments/46307923/capgAssignments/SRS_Array_Operations_Using_Pointer_V1.0/src/ArrPtr.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<myHeader.h>
+
+int main()
+{
+	int leng1,leng2,ch;
+	char str1[size];
+	char str2[size];
+	char *source,*dest;
+
+	source=str1;
+	dest=str2;
+
+	inputCharArray(source,dest);
+	leng1=strlen(str1);
+	leng2=strlen(str2);
+	str1[leng1-1]='\0';
+	str2[leng2-1]='\0';
+
+	printf("\n\nEnter the choice: ");
+	printf("\n1.Copystring from source to destination\n2.Concat source with destination\n3.Display source and Destination\n\n");
+	scanf("%d",&ch);
+
+	switch(ch)
+	{
+		case 1:
+			copystr(source,dest);
+			printf("Destination array after copyfunction :\n");
+			displayArray(dest);
+			break;
+
+		case 2:	
+
+			concatusinguf(leng1,source,dest);
+			printf("Source array after concat function :\n");
+			displayArray(source);
+			break;
+
+		case 3:
+			printf("\n\nSource array:\n");
+			displayArray(source);
+			printf("\n\nDestination array:\n");
+			displayArray(dest);
+			break;
+
+		default:
+			printf("\n\nInvalid option!!\n\n");
+	}
+	return EXIT_SUCCESS;
+}
