4dd6f11a235a4c0e072adcebbd06acbe93aea330
Vrajesh Alaguraj <31989953+VrajeshAlaguraj@users.noreply.github.com>
2023-03-03 09:11:19 +0530

    Updated

---
+++ Assigments/46307919/Mandatory_Assignments/N_Basic_Assign_2c.c
@@ -0,0 +1,93 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct personal
+{
+	char name[50];
+	char sex;
+	int age;
+	double salary;
+};
+
+typedef struct personal PERSONAL;
+
+char name[50];
+char sex;
+int age;
+double salary;
+
+int inputDataFunction();
+
+PERSONAL * inputStructData();
+void displayWOStruct();
+void displayDataFunction(PERSONAL *);
+
+int main()
+{
+	PERSONAL *data;
+	data=(PERSONAL *)malloc(1*sizeof(PERSONAL));
+
+	inputDataFunction();
+	displayWOStruct();
+	data=inputStructData();
+	displayDataFunction(data);
+	
+	free(data);
+
+	return EXIT_SUCCESS;
+}
+
+int inputDataFunction()
+{
+	int strl=0;
+	printf("\n\nEnter your name:\n");
+	fgets(name,50,stdin);
+	strl=strlen(name);
+	name[strl-1]='\0';
+
+	printf("\nEnter your gender(M/F/O): ");
+	scanf("%c",&sex);
+
+	printf("\nEnter your age(must be >0): ");
+	scanf("%d",&age);
+
+	if(age<=0)
+	{
+		printf("\n\nInvalid credentials!!\n\n");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("\nEnter your salary: ");
+	scanf("%lf",&salary);
+	
+	return EXIT_SUCCESS;
+}
+void displayWOStruct()
+{
+	printf("\n\nDisplaying without using structure:\n\n");
+	printf("\n\nName: %s\nGender: %c\nAge: %d\nMonthly Salary: %.2lf\n\n",name,sex,age,salary);
+}
+
+PERSONAL * inputStructData()
+{
+	PERSONAL *data;
+
+	data=(PERSONAL *)malloc(1*sizeof(PERSONAL));
+
+	strcpy(data->name,name);
+	data->sex=sex;
+	data->age=age;
+	data->salary=salary;
+
+	return data;
+}
+
+void displayDataFunction(PERSONAL *data)
+{
+	printf("\n\nThe details are: \n\n");
+	printf("Name: %s\n",data->name);
+	printf("Gender: %c\n",data->sex);
+	printf("Age: %d\n",data->age);
+	printf("Monthly Salary: %.2lf\n\n",data->salary);
+}
