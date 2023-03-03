b37cda392aa29f8f3473ae36296deebe6d9732c7
havisram <havis.prole@gmail.com>
2023-03-03 02:50:20 +0000

    Operations using pointer updated

---
+++ Assigments/46307915/capgassig/SRS_Array_Operations_Using_Pointer_V1.0/inc/myheader.h
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void copystr(char *,char *);
+void updatestr(char *,char ,int);
+void concatstr(char *,char *);
+void displaystr(char *);
