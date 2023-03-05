62c994b09febd9b0168abf09af0572d015e9140a
Princ398ku <kprince82549@gmail.com>
2023-03-05 06:17:35 +0000

    updated

---
+++ Assigments/46308300/Assignment/SRS_Array_Operations_Using_Pointer_V1.0/inc/myHeader.h
@@ -0,0 +1,8 @@
+#include<string.h>
+#define size 1024
+
+void inputCharArray(char *,char *);
+void copystr(char *,char *);
+void concatusinguf(int,char *,char *);
+void displayArray(char *);
+
---
+++ Assigments/46308300/Assignment/SRS_Array_Operations_Using_Pointer_V1.0/src/myfunctions.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<myHeader.h>
+
+
+void inputCharArray(char *source,char *dest)
+{
+	printf("\n\nEnter the string 1:\n");
+	fgets(source,size,stdin);
+
+	printf("\n\nEnter the string 2:\n");
+	fgets(dest,size,stdin);
+}
+
+void copystr(char *source,char *dest)
+{
+	int i=0;
+
+	for(i=0;*(source+i)!='\0';i++)
+	{
+		strcpy((dest+i),(source+i));
+	}
+	*(dest+i)='\0';
+}
+
+void concatusinguf(int leng1,char *source, char *dest)
+{
+	int i=0,j=0;
+	for(i=leng1-1;*(dest+j)!='\0';i++,j++)
+	{
+		strcpy((source+i),(dest+j));
+	}
+}
+
+void displayArray(char *tt)
+{
+	printf("\n\n%s\n\n",tt);
+#include<stdio.h>
+#include<myHeader.h>
+
+
+void inputCharArray(char *source,char *dest)
+{
+	printf("\n\nEnter the string 1:\n");
+	fgets(source,size,stdin);
+
+	printf("\n\nEnter the string 2:\n");
+	fgets(dest,size,stdin);
+}
+
+void copystr(char *source,char *dest)
+{
+	int i=0;
+
+	for(i=0;*(source+i)!='\0';i++)
+	{
+		strcpy((dest+i),(source+i));
+	}
+	*(dest+i)='\0';
+}
+
+void concatusinguf(int leng1,char *source, char *dest)
+{
+	int i=0,j=0;
+	for(i=leng1-1;*(dest+j)!='\0';i++,j++)
+	{
+		strcpy((source+i),(dest+j));
+	}
+}
+
+void displayArray(char *tt)
+{
+	printf("\n\n%s\n\n",tt);
+}
