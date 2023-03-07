80b8803f17a4f346ccad0ce26be4d28332d630a0
Shreedhar29 <Shreedharvss@gmail.com>
2023-03-07 09:56:31 +0000

    update

--- Assigments/46293506/CapGassignments/nalanda/C_Basic_assign-3b/prog.c
+++ Assigments/46293506/CapGassignments/nalanda/C_Basic_assign-3b/prog.c
@@ -7,51 +7,64 @@
 #define MAX_LEN 20
 
 int getNames(char [][MAX_LEN]);
-int printNames(char [][MAX_LEN]);
+int displayNames(char [][MAX_LEN]);
 int swap(char [][MAX_LEN],int);
-int isVowel(char [][MAX_LEN]);
+int isVowel(char );
+int hasVowel(char [][MAX_LEN],int);
+char rmVowel(char (*)[MAX_LEN],int);
 
 int main()
 {
 	char name[row][MAX_LEN];
 	int index;
 	int ch=1,ch1;
+
+	printf("Enter 4  Names\n");
 	getNames(name);
-	isVowel(name);
-/*	while(ch)
+	
+	while(ch)
 	{
-		printf("Enter \n 1.printNames \n 2.swapNames \n 3.exit \n");
+		printf("Enter \n 1.printNames \n 2.swapNames \n 3.isVowel or Not\n4.Remove Vowel\n5.exit \nchoice: ");
 		scanf("%d",&ch1);
 		switch(ch1)
 		{
 		
-			case 1:printNames(name);
+			case 1:displayNames(name);
 			       break;
-			case 2:printf("Enter a index value(0-3) to swap\n");
+			case 2:printf("Enter a row value(0-3) to swap\n");
 			       scanf("%d",&index);
 	                       swap(name,index);
 			       break;
-			case 3:return EXIT_FAILURE;
+			case 3:printf("Enter a row value of word to  know the isVowel\n");
+			       scanf("%d",&index);
+			       hasVowel(name,index);
+			       break;
+                        case 4:printf("Enter the row value to remove Vowels from a word\n");
+			       scanf("%d",&index);
+			       rmVowel(name,index);
+			       break;
+			case 5:exit(EXIT_SUCCESS);
+
 
 			default:printf("Wrong choice\n");
 		} 
       		
 		printf("Do you want to continue Y/N(1/0)\n");
 		scanf("%d",&ch);
-	}*/
+	}
 	return 0;
 }
 
 int getNames(char (*name)[MAX_LEN])
 {
 	int i;
-	for(i=0;i<1;i++)
+	for(i=0;i<row;i++)
 	{
 		fgets(name[i],1024,stdin);
 	}
 }
 
-int printNames(char (*name)[MAX_LEN])
+int displayNames(char (*name)[MAX_LEN])
 {
 	int i;
 	for(i=0;i<row;i++)
@@ -71,24 +84,61 @@ int swap(char (*name)[MAX_LEN],int index)
 	printf("ELements Swapped\n");
 }
 	
-int isVowel(char (*name)[MAX_LEN])
+int isVowel(char name)
+{
+        int flag = 0;
+	if(name=='a' || name=='e' || name=='i'
+				||name=='o' || name=='u')
+		{
+			flag=1;
+		}
+		
+	return flag;
+}
+
+int hasVowel(char (*name)[MAX_LEN],int index)
+{
+	int i,j;
+	int flag =0;
+	for(i=0;i<row;i++)
+	{
+		flag=0;
+		if(i==index)
+		{
+			for(j=0;j<strlen(name[i]);j++)
+		        {
+				flag=isVowel(name[i][j]);
+				if(flag ==1)
+				{
+					break;
+				}
+			}
+			if(flag ==1)
+				printf("\n%s has Vowels",name[i]);
+			else
+				printf("\n%s it doesnt have any Vowels",name[i]);
+		  }    
+		
+	}
+	return flag;
+}
+
+char rmVowel(char (*name)[MAX_LEN],int index)
 {
 	int i,j;
 	int flag=0;
-	for(i=0;i<1;i++)
+	for(i=0;i<row;i++)
 	{
-		for(j=0;j<strlen(name[i])-1;j++)
+		if(i==index)
 		{
-			if((strcmp(name[i],"a")==0)||(strcmp(name[i],"e")==0)
-		           ||(strcmp(name[i],"o")==0)||(strcmp(name[i],"i")==0)
-		             || (strcmp(name[i],"u")==0))
-			{
-				printf("%c\n",name[i][j]);
-			        flag=1;
-							
-			}
-		 }
+			for(j=0;j<strlen(name[i]);j++)
+	                {   
+				flag = isVowel(name[i][j]);
+			        if(flag!=1)
+					printf("%c",name[i][j]);
+		         }   
+		}
 	}
-	printf("%d",flag);
-	return flag;
 }
+
+	
---
+++ Assigments/46293506/CapGassignments/nalanda/C_LL_assign-5/linkedList.c
@@ -0,0 +1,101 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+struct word
+{
+	char word[20];
+	struct word *nlink;
+};
+typedef struct word WORD;
+
+WORD *getWord(WORD *,WORD *);
+void displayList(WORD *);
+
+int main()
+{
+
+	int ch1=1;
+        WORD *head=NULL;
+        WORD *temp=NULL;
+	while(ch1)
+	{
+		temp=getWord(temp,head);
+		if(head==NULL)
+			head=temp;
+		printf("do you want add more\n");
+		scanf("%d",&ch1);
+
+	}
+	temp =head;
+	displayList(temp);
+		
+
+	return 0;
+}
+
+WORD *getWord(WORD *temp,WORD *head,char word)
+{
+	WORD *newnode;
+	char *token;
+	
+	char word[1024];
+	FILE *filename;
+
+	filename =fopen("file.txt","r+");
+	
+	if(filename == NULL)
+	{
+		perror("file not opening\n");
+		exit(EXIT_FAILURE);
+	}
+	printf("File Opened Successully\n");
+	newnode = (WORD *)malloc(sizeof(WORD));
+
+	if(newnode ==NULL)
+	{
+		perror("Malloc Failed\n");
+		exit(EXIT_FAILURE);
+	}
+	fgets(word,1024,filename);
+        puts(word);
+	token = strtok(word," ");
+	strcpy(newnode->word,token);
+	newnode->nlink = NULL;
+
+	if(head==NULL)
+	{
+		head = newnode;
+		temp = newnode;
+	}
+	else
+	{
+		temp->nlink = newnode;
+		temp = temp->nlink;
+	}
+	printf("Word added\n");
+
+	return temp;
+}
+
+void displayList(WORD *temp)
+{
+	if(temp == NULL)
+	{
+		printf("List is Empty\n");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		printf("Words are\n");
+		while(temp!=NULL)
+		{
+			printf("%s ",temp->word);
+			if(temp->nlink==NULL)
+				break;
+			temp = temp->nlink;
+		}
+	}
+}
+
