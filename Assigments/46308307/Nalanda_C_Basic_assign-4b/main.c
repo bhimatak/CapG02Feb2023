7bdd6e387b2ff1e5a13ca026cd40ca85024e3c9f
sagar <sagar30663@gmail.com>
2023-03-09 16:31:26 +0000

    updated

---
+++ Assigments/46308307/Nalanda_C_Basic_assign-4b/main.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#define BUFF 1024
+
+int getFileStrings(char *);
+void display();
+
+char **str;
+int ll = 0;
+
+int main(int argc, char **argv)
+{
+	char *fileName = argv[1];
+	int s;
+
+	s = getFileStrings(fileName);
+	if(s == 0)
+	{
+		printf("\nData successfully read and stored in the double char pointer!");
+	}
+	else
+	{
+		printf("\nError occured while reading data or writing it to the double char pointer!");
+	}
+	printf("\n-----------Displaying the contents stored in the double char pointer---------------");
+	/*display();*/
+	return EXIT_SUCCESS;
+}
+
+int getFileStrings(char *filename)
+{
+	FILE *fptr;
+	char line[BUFF];
+	int i;
+
+	fptr = fopen(filename,"r");
+	if(fptr == NULL)
+	{
+		return EXIT_FAILURE;
+	}
+	while(fgets(line,BUFF,fptr)!=NULL)
+	{
+		ll++;
+	}
+	printf("\n%d\n",ll);
+	ll++;
+	rewind(fptr);
+	str = (char **)malloc(ll*sizeof(char *));
+	for(i=0;i<ll;i++)
+	{
+		str[i] = (char *)malloc(BUFF*sizeof(char));
+	}
+	while(fgets(line,BUFF,fptr)!=NULL) 
+	{
+		strcpy(str[i],line);
+	}
+	fclose(fptr);
+	return EXIT_SUCCESS;
+}
+void display()
+{
+	int i;
+
+	for(i=0;str[i]!=NULL;i++)
+	{
+		printf("\n%s\n",str[i]);
+	}
+	free(str);
+}
+
+
